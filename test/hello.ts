b629bec9edd9bc0f65749838a628fbafc59cb4a5
Reinkaos <ds2lvg@gmail.com>
2017-09-15 07:59:23 +0900

    컴파일된 js 파일도 같이 업로드

--- test/hello.ts
+++ test/hello.ts
@@ -1,7 +1,6 @@
+// typescript 컴파일해서 .js 파일도 같이 업로드 해보기
 alert("Hello Github");
-alert("Hello Github");
-console.log("hello world");
-console.log("hello world");
+console.log("Hello Typescript");
 /*
 CMD에서 깃사용
 git status : 현재 변경 상태보기
@@ -10,13 +9,10 @@ git status : modified 됬나 상태 확인
 git commit -m "메세지" : 커밋 메시지 작서
 git push : 올리기
 */
-
 /*
-15.09.15 다시 도전
 VS CODE에서 깃사용
 1. 폴더 열기로 푸시할 폴더부터 선택
 2. 깃 눌러서 변경내용에 있는 파일 "변경 내용 스테이징"
 3. 커밋 시키기
 4. 푸시 시키기
-이거 꼬이면 왜 꼬이는지 모르겠음
 */
