0e557618dabfcbffc9b533c378e4a193e365ad06
Reinkaos <ds2lvg@gmail.com>
2017-09-14 21:34:50 +0900

    커밋 시킬때 메시지 작성해야되네 귀찮아..

--- test/hello.js
+++ test/hello.js
@@ -1,3 +1,8 @@
-alert("Hello world");
+alert("Hello Github");
 console.log("hello world");
-console.log("hello world");ㄴ
+/*
+1. 폴더 열기로 푸시할 폴더부터 선택
+2. 깃 눌러서 변경내용에 있는 파일 "변경 내용 스테이징"
+3. 커밋 시키기
+4. 푸시 시키기
+*/
