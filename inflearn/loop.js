7c90d5bed7452b0437f94e3eff9340c6ed5573ac
Reinkaos <ds2lvg@gmail.com>
2017-09-15 10:08:50 +0900

    반복문

---
+++ inflearn/loop.js
@@ -0,0 +1,11 @@
+/* for */
+var myArray = ["A", "B", "C"];
+for (var i = 0; i < myArray.length; i++) {
+    console.log("index : " + i + " -> " + myArray[i]);
+}
+/* while */
+var i = 10;
+while (i > 0) {
+    console.log(i + "left");
+    i -= 1;
+}
---
+++ inflearn/loop.ts
@@ -0,0 +1,13 @@
+/* for */
+var myArray: string[] = ["A", "B", "C"];
+
+for (var i=0; i<myArray.length; i++) {
+    console.log("index : " + i + " -> " + myArray[i]);
+}
+
+/* while */
+var i = 10;
+while (i>0){
+    console.log(i + "left");
+    i -= 1;
+}
