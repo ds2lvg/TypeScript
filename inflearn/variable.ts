39bbc0a76dd17055258085c18fd2fa53ced3bf38
Reinkaos <ds2lvg@gmail.com>
2017-09-15 09:26:24 +0900

    변수

---
+++ inflearn/variable.ts
@@ -0,0 +1,32 @@
+/*** Variable(변수)
+var name : type = value;
+
+4가지 타입 논리(boolean), 숫자(number), 문자(string), 미정(any)
+*/
+var isDone : boolean = false;
+var age : number = 42;
+var myName: string = "Anders";
+var notSure : any = "아무거나 써도 됨"
+
+console.log(isDone);
+console.log(age);
+console.log(myName);
+console.log(notSure);
+
+/*** null vs undefined
+null : 변수 값이 비어있음 = object이지만 빈 값
+undefined : 값 자체가 없음 = 자료형 조차 결정되지 않음
+*/
+var emptyVariable = null;
+var newvariable;
+
+console.log(emptyVariable);
+console.log(newvariable);
+
+/* collection(배열) 
+자료형은 객체
+타입스크립트는 배열 선언하는 방법이 두가지
+ */
+
+ var list : number[] = [1, 2, 3];
+ var list : Array<number> = [1, 2, 3];
