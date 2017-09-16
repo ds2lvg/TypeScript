b1e1fb6686db54d8ad7b81082e99da5c23cbc21a
Reinkaos <ds2lvg@gmail.com>
2017-09-16 10:28:51 +0900

    클래스

---
+++ inflearn/class.ts
@@ -0,0 +1,53 @@
+/** class
+ * 데이터(변수)와 로직(함수)
+ * 
+ * 인터페이스와 차이점
+ * constructor(생성자) : 클래스 호출될때 실행되는 함수
+ * 함수의 구체적인 내용을 꼭 써야함
+ */
+
+ class Point_c {
+     x: number;
+
+    // public 키워드 때문에 point 클래스 영역에서 y 사용가능
+     constructor(x: number, public y: number = 0) {
+         // 클래스의 멤버 x = 매개변수 x
+         this.x = x;
+     }
+
+     dist() {
+         return Math.sqrt(this.x * this.x + this.y * this.y);
+     }
+     // 매개변수 값 안적어주면 0이 들어가게 초기값 셋팅
+     static origin = new Point_c(0, 0);
+ }
+
+var point_c1 = new Point_c(10, 20);
+var point_c2 = new Point_c(25);
+
+
+/* class를 interface를 이용해 구현하려면 
+
+interface Point_i {
+    x: number;
+    y: number;
+
+    dist(): number;
+}
+
+var origin: Point_i = {
+    x:0, y:0,
+    dist: () => Math.sqrt(this.x * this.x + this.y * this.y)
+};
+
+var point_i1: Point_i = {
+    x:10, y:20,
+    dist: () => Math.sqrt(this.x * this.x + this.y * this.y)
+};
+
+var point_i2: Point_i = {
+    x:25, y:0,
+    dist: () => Math.sqrt(this.x * this.x + this.y * this.y)
+};
+
+*/ 
